6c44265691f5142e213c4cfa3cf44c28078307fc
Dreambackie <121892381+Dreambackie@users.noreply.github.com>
2024-07-26 12:04:49 +0500

    Delete business-appliware directory

--- business-appliware/insert_category.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = new mysqli('localhost', 'root', '', 'business_directory');
-$category_name = $_POST['category_name'];
-$sql = "INSERT INTO categories (name) VALUES ('$category_name')";
-$conn->query($sql);
-header('Location: add_category.php');
-?>
--- business-appliware/insert_city.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = new mysqli('localhost', 'root', '', 'business_directory');
-$city_name = $_POST['city_name'];
-$sql = "INSERT INTO cities (name) VALUES ('$city_name')";
-$conn->query($sql);
-header('Location: add_city.php');
-?>
